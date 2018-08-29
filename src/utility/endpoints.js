7129c07d7996505fb6d328069ec530b27ecd4773
Wright <delwrig@gmail.com>
2018-08-29 15:52:12 -0400

    add BOT_GENERATE_TOKEN
    Signed-off-by: Wright <delwrig@gmail.com>

--- src/utility/endpoints.js
+++ src/utility/endpoints.js
@@ -1,6 +1,7 @@
 module.exports.BASE_URL = `https://discordbotlist.com/api`;
 
 module.exports.BOT_STATISTICS = (botId) => `${module.exports.BASE_URL}/bots/${botId}/stats`;
+module.exports.BOT_GENERATE_TOKEN = (botId) => `${module.exports.BASE_URL}/bots/${botId}/token`;
 module.exports.BOT_UPVOTES = (botId) => `${module.exports.BASE_URL}/bots/${botId}/upvotes`;
 module.exports.FETCH_BOT = (botId) => `${module.exports.BASE_URL}/bots/${botId}`;
 module.exports.FETCH_USER = (userId) => `${module.exports.BASE_URL}/users/${userId}`;
