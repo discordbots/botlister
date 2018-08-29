b4fa2f200f7066800e8fed32469543cf11d7b203
Wright <delwrig@gmail.com>
2018-08-29 15:48:04 -0400

    fix ADMIN_REFRESH_BOT endpoint
    Signed-off-by: Wright <delwrig@gmail.com>

--- src/utility/endpoints.js
+++ src/utility/endpoints.js
@@ -7,7 +7,7 @@ module.exports.FETCH_USER = (userId) => `${module.exports.BASE_URL}/users/${user
 module.exports.FETCH_ALL_BOTS = () => `${module.exports.BASE_URL}/bots`;
 module.exports.ADMIN_REFRESH_USER = (userId) => `${module.exports.BASE_URL}/users/${userId}/refresh`;
 module.exports.ADMIN_BAN_USER = (userId) => `${module.exports.BASE_URL}/users/${userId}/ban`;
-module.exports.ADMIN_BAN_BOT = (botId) => `${module.exports.BASE_URL}/bots/${botId}/refresh`;
+module.exports.ADMIN_REFRESH_BOT = (botId) => `${module.exports.BASE_URL}/bots/${botId}/refresh`;
 module.exports.ADMIN_VERIFY_BOT = (botId) => `${module.exports.BASE_URL}/bots/${botId}/verification`;
 module.exports.ADMIN_FETCH_UNINVITED_BOTS = (botId) => `${module.exports.BASE_URL}/bots/uninvited`;
 module.exports.ADMIN_FETCH_DBL_CONFIG = (botId) => `${module.exports.BASE_URL}/dbl/configurations`;
