a492e0b05def6ade05dbeaca02b6b13404c59f57
Wright <delwrig@gmail.com>
2018-08-29 20:40:11 -0400

    update endpoints
    Signed-off-by: Wright <delwrig@gmail.com>

--- src/utility/endpoints.js
+++ src/utility/endpoints.js
@@ -7,9 +7,3 @@ module.exports.FETCH_BOT = (botId) => `${module.exports.BASE_URL}/bots/${botId}`
 module.exports.FETCH_USER = (userId) => `${module.exports.BASE_URL}/users/${userId}`;
 module.exports.FETCH_ALL_BOTS = () => `${module.exports.BASE_URL}/bots`;
 module.exports.FETCH_MY_BOTS = () => `${module.exports.BASE_URL}/bots/mine`;
-module.exports.ADMIN_REFRESH_USER = (userId) => `${module.exports.BASE_URL}/users/${userId}/refresh`;
-module.exports.ADMIN_BAN_USER = (userId) => `${module.exports.BASE_URL}/users/${userId}/ban`;
-module.exports.ADMIN_REFRESH_BOT = (botId) => `${module.exports.BASE_URL}/bots/${botId}/refresh`;
-module.exports.ADMIN_VERIFY_BOT = (botId) => `${module.exports.BASE_URL}/bots/${botId}/verification`;
-module.exports.ADMIN_FETCH_UNINVITED_BOTS = (botId) => `${module.exports.BASE_URL}/bots/uninvited`;
-module.exports.ADMIN_FETCH_DBL_CONFIG = (botId) => `${module.exports.BASE_URL}/dbl/configurations`;
