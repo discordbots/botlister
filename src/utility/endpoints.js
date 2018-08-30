af8fa8994bcddd99a3666996ef97b469b4c04fc4
Wright <delwrig@gmail.com>
2018-08-30 13:10:40 -0400

    fix endpoint
    Signed-off-by: Wright <delwrig@gmail.com>

--- src/utility/endpoints.js
+++ src/utility/endpoints.js
@@ -9,7 +9,7 @@ module.exports.BOT = (botId) => `${module.exports.BASE_URL}/bots/${botId}`;
 module.exports.BOT_TOKEN = (botId) => `${module.exports.BASE_URL}/bots/${botId}/token`;
 module.exports.BOT_UPVOTES = (botId) => `${module.exports.BASE_URL}/bots/${botId}/upvotes`;
 module.exports.BOT_STATISTICS = (botId) => `${module.exports.BASE_URL}/bots/${botId}/stats`;
-module.exports.USER = (userId) => `${module.exports.BASE_URL}/bots/${botId}`;
+module.exports.USER = (userId) => `${module.exports.BASE_URL}/bots/${userId}`;
 /* Admin Endpoints */
 module.exports.ADMIN_BAN_USER = (userId) => `${module.exports.BASE_URL}/users/${userId}/ban`;
 module.exports.ADMIN_REFRESH_USER = (userId) => `${module.exports.BASE_URL}/users/${userId}/refresh`;
