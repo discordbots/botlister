4b8efd1f5cf619ee25e3db138944e426d7c888e7
Wright <delwrig@gmail.com>
2018-08-30 13:31:51 -0400

    fix
    Signed-off-by: Wright <delwrig@gmail.com>

--- src/structures/User.js
+++ src/structures/User.js
@@ -29,7 +29,7 @@ class User {
         this.discriminator = data.discriminator !== undefined ? data.discriminator : this.discriminator;
         this.admin = data.admin !== undefined ? data.admin : this.admin;
         this.banned = data.banned !== undefined ? data.banned : this.banned;
-        this.bots = data.bots !== undefined ? data.bots.map(bot => new Bot(bot, this._core)) : this.bots;
+        this.bots = data.bots !== undefined ? data.bots.map(b => new Bot(b, this._core)) : this.bots;
     }
 
     get defaultAvatarURL() {
