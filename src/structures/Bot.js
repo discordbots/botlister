8359491050f057f12878b82f014a627078c8b9aa
Yikey <gtc122004@gmail.com>
2018-10-08 02:56:17 +0000

    Code style Improvements (#7)
    Replace ternaries with OR conditions

--- src/structures/Bot.js
+++ src/structures/Bot.js
@@ -34,20 +34,18 @@ class Bot {
     }
 
     update(data) {
-        this.avatar = data.avatar !== undefined ? data.avatar : this.avatar;
-        this.username = data.username !== undefined ? data.username : this.username;
-        this.discriminator = data.discriminator !== undefined ? data.discriminator : this.discriminator;
-        this.shortDescription = (data.short_description !== (undefined || '')) ? data.short_description : this.shortDescription;
-        this.longDescription = (data.long_description !== (undefined || '')) ? data.long_description : this.longDescription;
-        this.prefix = (data.prefix !== (undefined || '')) ? data.prefix : this.prefix;
-        this.website = (data.website !== (undefined || '')) ? data.website : this.website;
-        this.botInvite = (data.bot_invite !== (undefined || '')) ? data.bot_invite : this.botInvite;
-        this.serverInvite = (data.server_invite !== (undefined || '')) ? data.server_invite : this.serverInvite;
-        this.verified = (data.verified !== undefined) ? data.verified : this.verified;
-        this.upvotes = (data.upvotes !== undefined) ? data.upvotes : this.upvotes;
-        this.upvoted = (data.is_upvoted !== undefined) ? data.is_upvoted : this.upvoted;
-        this.owner = (data.owner !== undefined) ? (new User(data.owner, this._core)) : this.owner;
-        this.statistics = (data.stats !== undefined) ? data.stats : this.statistics;
+        this.avatar = data.avatar || this.avatar;
+        this.username = data.username || this.username;
+        this.discriminator = data.discriminator || this.discriminator;
+        this.shortDescription = data.short_description || this.shortDescription;
+        this.longDescription = data.long_description || this.longDescription;
+        this.prefix = data.prefix || this.prefix;
+        this.website = data.website || this.website;
+        this.botInvite = data.bot_invite || this.botInvite;
+        this.serverInvite = data.server_invite || this.serverInvite;
+        this.verified = data.verified || this.verified;
+        this.upvotes = data.upvotes || this.upvotes;
+        this.upvoted = data.is_upvoted || this.upvoted;
     }
 
     get defaultAvatarURL() {
